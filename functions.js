4719c4e5c01a044b73fc42479c6c08c6e2b004d4
NaserAldeen <30509@aum.edu.kw>
2019-09-15 10:24:02 +0300

    first commit

--- functions.js
+++ functions.js
@@ -6,6 +6,13 @@
  */
 function greet(name) {
   // Your code here
+  if (name == null) {
+    
+    console.log("Hello") 
+  }
+  else {
+    console.log("Hello "+name)
+  }
 }
 
 /**
@@ -15,6 +22,12 @@ function greet(name) {
  */
 function isOdd(n) {
   // Your code here
+  if (n % 2 === 0) {
+    return false
+  }
+  else {
+    return true
+  }
 }
 
 /**
@@ -30,6 +43,8 @@ function isOdd(n) {
  */
 function oddsSmallerThan(n) {
   // Your code here
+
+  return Math.abs(Math.round((n-1)/2))
 }
 
 /**
@@ -44,6 +59,12 @@ function oddsSmallerThan(n) {
  */
 function squareOrDouble(n) {
   // Your code here
+  if (n % 2 === 0) {
+    return n*2
+  }
+  else {
+    return n*n
+  }
 }
 
 /**
@@ -64,8 +85,19 @@ function squareOrDouble(n) {
  */
 function ageFromBirthDate(birthDate) {
   const _MS_PER_YEAR = 1000 * 60 * 60 * 24 * 365;
-
+  let year = birthDate.substring(0,4)
+  let month = birthDate.substring(4,6)
+  let day = birthDate.substring(6,8)
+  console.log(year)
+  console.log(month)
+  console.log(day)
   // Your code here
+  var d1 = new Date(month+"/"+day+"/"+year);
+	var d2 = new Date().getTime();
+  var timeDiff = d2 - d1.getTime();
+  var yearss = timeDiff / _MS_PER_YEAR
+
+  return Math.floor(yearss)
 }
 
 module.exports = {
